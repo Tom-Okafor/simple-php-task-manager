79153b5849cc4c5e72119866023e10aea5358b74
Tom Okafor <tmeka008@gmail.com>
2025-08-24 23:19:02 +0100

    feat: create and style header

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
     <title>Task Manager</title>
 </head>
 <body>
+    <header>
+        <img src="./assets/hourglass-svgrepo-com.svg" alt="taskit logo">
+        <span>taskIT</span>
+    </header>
+    <main></main>
+    <footer></footer>
     
 </body>
 </html>
