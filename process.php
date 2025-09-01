9f2fd9a201b3f78e8d0ed09c5ca6047e863669ef
Tom Okafor <tmeka008@gmail.com>
2025-09-01 17:32:29 +0100

    refactor: rewrite if statements and implement using the header function to reload the page to avoid resubmission of form on page refresh

--- process.php
+++ process.php
@@ -1,21 +1,32 @@
 <?php
 
-session_start();
-if (!isset($_SESSION['task_data'])) {
-    $_SESSION['task_data'] = [];
-}
-if (isset($_POST['submit_task'])) {
+
+function processTaskSubmission()
+{
+    session_start();
+    if (!($_SERVER['REQUEST_METHOD'] == 'POST')) {
+        return;
+    }
+
+    if (!isset($_SESSION['task_data'])) {
+        $_SESSION['task_data'] = [];
+    }
+
     $task_name = $_POST['task_name'];
     $task_priority = $_POST['task_priority'];
     $task_description = $_POST['task_description'];
     if (empty($task_name) || empty($task_priority) || empty($task_description)) {
         $task_data_input_error = true;
-    } else {
-        array_push($_SESSION['task_data'], [
-            "task_id" => count($_SESSION['task_data']) + 1,
-            "task_name" => $task_name,
-            "task_description" => $task_description,
-            "task_priority" => $task_priority,
-            "task_status" => 'incomplete' ]);
+        return;
     }
+    array_push($_SESSION['task_data'], [
+        "task_id" => count($_SESSION['task_data']) + 1,
+        "task_name" => $task_name,
+        "task_description" => $task_description,
+        "task_priority" => $task_priority,
+        "task_status" => 'incomplete' ]);
+
+    header('Location: index.php');
 }
+
+processTaskSubmission();
